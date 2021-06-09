3aa7d702702d6debbb2535c04fe1639fc41bdd30
吉岡寿輝 <yoshiokahisaki@yoshiokahisakinoMacBook-Pro-2.local>
2021-06-09 18:05:40 +0900

    init

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,25 @@
+/*
+ * ====================================================================
+ * Grow Template
+ * @package  Grow Template
+ * @author   GrowGroup.Inc <info@grow-group.jp>
+ * @license  MIT Licence
+ * ====================================================================
+ */
+
+(function ($) {
+  var growApp = function () {
+
+  };
+
+  /************************
+   * please insert your code
+   *************************/
+  growApp.prototype.myCode = function () {
+  }
+
+  $(function () {
+    var app = new growApp();
+    app.myCode();
+  });
+})(jQuery);
